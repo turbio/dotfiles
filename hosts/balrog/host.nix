e8fbfb1a22ae6142231d3aaf95218b6e9d5c4585
turbio <git@turb.io>
2021-07-16 13:01:22 -0700

    loki

--- hosts/balrog/host.nix
+++ hosts/balrog/host.nix
@@ -126,4 +126,104 @@ in
     enable = true;
     web.listen-address = "127.0.0.1:9091";
   };
+
+  systemd.services.promtail = {
+    description = "Promtail service for Loki";
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = ''
+        ${pkgs.grafana-loki}/bin/promtail --config.file ${(pkgs.writeText "promtail.yaml" (builtins.toJSON {
+          server = {
+            http_listen_port = 20183;
+            grpc_listen_port = 0;
+          };
+          positions = { filename = "/tmp/positions.yaml";};
+          clients = [{ url = "http://127.0.0.1:3100/loki/api/v1/push"; }];
+          scrape_configs = [
+            {
+              job_name = "journal";
+              journal = { max_age = "12h"; labels = { job = "systemd-journal"; host = "balrog"; };};
+              relabel_configs = [{ source_labels = [ "__journal__systemd_unit" ]; target_label = "unit"; }];
+            }
+          ]
+            ;
+        }))}
+      '';
+    };
+  };
+
+  services.loki = {
+    enable = true;
+    configuration = {
+      auth_enabled = false;
+      server = {
+        http_listen_port = 3100;
+        http_listen_address = "127.0.0.1";
+      };
+      ingester = {
+        chunk_idle_period = "1h";
+        max_chunk_age = "1h";
+        chunk_target_size = 1048576;
+        chunk_retain_period = "30s";
+        max_transfer_retries = 0;
+        lifecycler = {
+          address = "0.0.0.0";
+          ring = {
+            kvstore = {
+              store = "inmemory";
+            };
+            replication_factor = 1;
+          };
+          final_sleep = "0s";
+        };
+      };
+
+      storage_config = {
+        boltdb_shipper = {
+          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
+          cache_location = "/var/lib/loki/boltdb";
+          cache_ttl = "24h";
+          shared_store = "filesystem";
+        };
+        filesystem = {
+          directory = "/var/lib/loki/chunks";
+        };
+      };
+
+      limits_config = {
+        reject_old_samples = true;
+        reject_old_samples_max_age = "168h";
+      };
+
+      chunk_store_config = {
+        max_look_back_period = "0s";
+      };
+
+      table_manager = {
+        retention_deletes_enabled = false;
+        retention_period = "0s";
+      };
+
+      compactor = {
+        working_directory = "/tmp/loki/boltdb-shipper-compactor";
+        shared_store = "filesystem";
+      };
+
+      schema_config = {
+        configs = [
+          {
+            from = "2020-10-24";
+            store = "boltdb-shipper";
+            object_store = "filesystem";
+            schema = "v11";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+      };
+    };
+  };
 }

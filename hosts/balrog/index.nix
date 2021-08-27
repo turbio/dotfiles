1f650b3b10d4f53fc4cc2a4ab8739e2020735a7f
turbio <mason@turb.io>
2021-08-26 18:32:45 -0700

    update index + 404ing

--- hosts/balrog/index.nix
+++ hosts/balrog/index.nix
@@ -1,26 +1,55 @@
 { config, pkgs, lib, ... }:
 let
-  html = (pkgs.writeTextDir "index.html" ''
+  root = (pkgs.writeTextDir "index.txt" ''
     Hey!
     ====
 
+    I'm Turbio 🐧
+    I'm very offline
+    This is really the extent of my web presence
 
-    I'm Turbio
-    🐧
+    If ya wanna say hi:
+      - Email me: anything @ this domain
+      - Hmu on discord: turbio eight three six three
+      - Or better yet: dots.turb.io
+
+    ヾ(*ФωФ)βyё βyё☆彡
+  '');
+
+  vhost404 = (pkgs.writeTextDir "index.txt" ''
+    404!
+    ====
+
+    lol
   '');
 in
 {
   services.nginx.virtualHosts."turb.io" = {
     addSSL = true;
     enableACME = true;
-    default = true;
 
-    root = "${html}";
+    root = "${root}";
+
+    locations."/" = {
+      index = "index.txt";
+    };
+
     extraConfig = ''
       charset utf-8;
-      types {
-          text/plain  html;
-      }
+    '';
+  };
+
+  services.nginx.virtualHosts."404.turb.io" = {
+    addSSL = true;
+    enableACME = true;
+    default = true;
+
+    locations."/" = { return = "404"; };
+
+    root = "${vhost404}";
+    extraConfig = ''
+      error_page 404 /index.txt;
+      charset utf-8;
     '';
   };
 }

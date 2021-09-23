ae756a8220c1960bbd7c22161a26c2ff874ee628
turbio <git@turb.io>
2021-09-22 21:36:09 -0700

    i just wanna chat

--- packages.nix
+++ packages.nix
@@ -1,62 +1,74 @@
-{ pkgs, ... }:
-let mars-ide =
-  pkgs.callPackage
-    ({ lib, stdenvNoCC, fetchurl, makeWrapper, copyDesktopItems, makeDesktopItem, unzip, imagemagick, jre }:
+{ pkgs, fetchurl, ... }:
+let
+  mars-ide =
+    pkgs.callPackage
+      ({ lib, stdenvNoCC, fetchurl, makeWrapper, copyDesktopItems, makeDesktopItem, unzip, imagemagick, jre }:
 
-      stdenvNoCC.mkDerivation rec {
-        pname = "mars-mips";
-        version = "4.5";
+        stdenvNoCC.mkDerivation rec {
+          pname = "mars-mips";
+          version = "4.5";
 
-        src = fetchurl {
-          url = "https://courses.missouristate.edu/KenVollmar/MARS/MARS_${lib.replaceStrings ["."] ["_"] version}_Aug2014/Mars${lib.replaceStrings ["."] ["_"] version}.jar";
-          sha256 = "15kh1fahkkbbf4wvb6ijzny4fi5dh4pycxyzp5325dm2ddkhnd5c";
-        };
+          src = fetchurl {
+            url = "https://courses.missouristate.edu/KenVollmar/MARS/MARS_${lib.replaceStrings ["."] ["_"] version}_Aug2014/Mars${lib.replaceStrings ["."] ["_"] version}.jar";
+            sha256 = "15kh1fahkkbbf4wvb6ijzny4fi5dh4pycxyzp5325dm2ddkhnd5c";
+          };
 
-        dontUnpack = true;
+          dontUnpack = true;
 
-        nativeBuildInputs = [ makeWrapper copyDesktopItems unzip imagemagick ];
+          nativeBuildInputs = [ makeWrapper copyDesktopItems unzip imagemagick ];
 
-        desktopItems = [
-          (makeDesktopItem {
-            name = pname;
-            desktopName = "MARS";
-            exec = "mars-mips";
-            icon = "mars-mips";
-            comment = "An IDE for programming in MIPS assembly language";
-            categories = "Development;IDE;";
-          })
-        ];
+          desktopItems = [
+            (makeDesktopItem {
+              name = pname;
+              desktopName = "MARS";
+              exec = "mars-mips";
+              icon = "mars-mips";
+              comment = "An IDE for programming in MIPS assembly language";
+              categories = "Development;IDE;";
+            })
+          ];
 
-        installPhase = ''
-          runHook preInstall
-          export JAR=$out/share/java/${pname}/${pname}.jar
-          install -D $src $JAR
-          makeWrapper ${jre}/bin/java $out/bin/${pname} \
-            --add-flags "-jar $JAR"
-          unzip ${src} images/MarsThumbnail.gif
-          mkdir -p $out/share/pixmaps
-          convert images/MarsThumbnail.gif $out/share/pixmaps/mars-mips.png
-          runHook postInstall
-        '';
+          installPhase = ''
+            runHook preInstall
+            export JAR=$out/share/java/${pname}/${pname}.jar
+            install -D $src $JAR
+            makeWrapper ${jre}/bin/java $out/bin/${pname} \
+              --add-flags "-jar $JAR"
+            unzip ${src} images/MarsThumbnail.gif
+            mkdir -p $out/share/pixmaps
+            convert images/MarsThumbnail.gif $out/share/pixmaps/mars-mips.png
+            runHook postInstall
+          '';
 
-        meta = with lib; {
-          description = "An IDE for programming in MIPS assembly language intended for educational-level use";
-          homepage = "https://courses.missouristate.edu/KenVollmar/MARS/";
-          license = licenses.mit;
-          maintainers = with maintainers; [ angustrau ];
-          platforms = platforms.all;
-        };
-      })
-    { };
+          meta = with lib; {
+            description = "An IDE for programming in MIPS assembly language intended for educational-level use";
+            homepage = "https://courses.missouristate.edu/KenVollmar/MARS/";
+            license = licenses.mit;
+            maintainers = with maintainers; [ angustrau ];
+            platforms = platforms.all;
+          };
+        })
+      { };
+  discord-override = pkgs.callPackage ./discord.nix rec {
+    pname = "discord";
+    binaryName = "Discord";
+    desktopName = "Discord";
+    version = "0.0.16";
+    src = fetchurl {
+      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+      sha256 = "UTVKjs/i7C/m8141bXBsakQRFd/c//EmqqhKhkr1OOk=";
+    };
+  };
 in
 {
   desktop = with pkgs; [
+    discord-override
+
     nixpkgs-fmt
     firefox-wayland
     chromium
     alacritty
     pavucontrol
-    discord
     blueberry
     spotify
     mako # notification daemon

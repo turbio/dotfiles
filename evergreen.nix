b97facddada59ac77ba78b7666568c2a96583876
turbio <mason@turb.io>
2021-11-17 21:52:16 -0800

    bring in those alerts

--- evergreen.nix
+++ evergreen.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
-let script = pkgs.writeShellScript "evergreenerr" ''
+let script = pkgs.writeShellScript "evergreener" ''
   export PATH=${pkgs.bash}/bin:$PATH
   export PATH=${pkgs.glib}/bin:$PATH
   export PATH=${pkgs.sudo}/bin:$PATH
@@ -7,6 +7,7 @@ let script = pkgs.writeShellScript "evergreenerr" ''
   export PATH=${pkgs.util-linux}/bin:$PATH
   export PATH=${pkgs.libnotify}/bin:$PATH
   export PATH=${pkgs.nixos-rebuild}/bin:$PATH
+  export PATH=${./bin}:$PATH
 
   ${./bin/evergreen}
 '';

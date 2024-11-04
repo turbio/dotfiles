e6c051302287a9897bcb6f12cb9a9a28b5e7cb42
turbio <git@turb.io>
2024-11-04 04:12:45 -0600

    clean up ballos

--- hosts/ballos/configuration.nix
+++ hosts/ballos/configuration.nix
@@ -139,25 +139,6 @@
         };
     })
 
-    /*
-    (final: prev: {
-      python-matter-server = prev.python-matter-server.overrideAttrs (finalAttrs: prevAttrs: {
-        version = "5.0.3";
-        src = prev.fetchFromGitHub {
-          owner = "home-assistant-libs";
-          repo = "python-matter-server";
-          rev = "refs/tags/5.0.3";
-          hash = "sha256-bR6AVoy9f02RKZ57dnHTDAv5LTCcd/qBbzMDRKsGbfM=";
-        };
-        patches = null;
-        postPatch = ''
-          substituteInPlace pyproject.toml \
-            --replace 'version = "0.0.0"' 'version = "5.0.3"'
-        '';
-      });
-    })
-    */
-
     (final: prev: {
       freeipmi = prev.freeipmi.overrideAttrs (finalAttrs: prevAttrs: {
         configureFlags = prevAttrs.configureFlags ++ [ "ac_dont_check_for_root=yes" ];
@@ -276,21 +257,6 @@
           { targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.apcupsd.port}" ]; }
         ];
       }
-      # {
-      #   job_name = "idrac";
-      #   scrape_timeout = "1m";
-      #   scrape_interval = "2m";
-      #   static_configs = [{
-      #     # F
-      #     targets = [ "idrac-6970JH2.local" ];
-      #     #targets = [ "192.168.3.203" ];
-      #   }];
-      #   relabel_configs = [
-      #     { source_labels = [ "__address__" ]; target_label = "__param_target"; }
-      #     { source_labels = [ "__param_target" ]; target_label = "instance"; }
-      #     { target_label = "__address__"; replacement = "127.0.0.1:${toString config.services.prometheus.exporters.idrac.port}"; }
-      #   ];
-      # }
       {
         job_name = "smartctl";
         static_configs = [
@@ -346,29 +312,6 @@
       apcupsd = {
         enable = true;
       };
-      # idrac = {
-      #   enable = true;
-      #   configuration = {
-      #     timeout = 60;
-      #     listenAddress = "127.0.0.1";
-      #     port = 9348;
-      #     hosts = {
-      #       "idrac-6970JH2.local" = {
-      #         username = "root";
-      #         password = "calvin";
-      #       };
-      #     };
-      #     metrics = {
-      #       system = true;
-      #       sensors = true;
-      #       power = true;
-      #       storage = true;
-      #       memory = true;
-      #       network = true;
-      #       sel = true;
-      #     };
-      #   };
-      # };
     };
   };
 
@@ -386,9 +329,6 @@
     options = [ "nofail" ];
   };
 
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.efiSysMountPoint = "/efi";
-
   #boot.uki.settings.UKI.Cmdline = "init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}";
   #boot.loader.external = let arch = pkgs.stdenv.hostPlatform.efiArch; in {
   #  enable = true;
@@ -402,104 +342,4 @@
   #    #cp ${config.system.build.uki}/${config.system.boot.loader.ukiFile} /efi/EFI/Linux/${config.system.boot.loader.ukiFile}
   #  ;
   #};
-
-
-  # Use the GRUB 2 boot loader.
-  #boot.loader.grub.enable = true;
-  #boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     thunderbird
-  #   ];
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 }

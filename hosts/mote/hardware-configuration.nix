1ad2298113a9431c43b78fc75f55ebc50174077e
turbio <git@turb.io>
2026-01-17 10:14:28 -0600

    yikers

--- hosts/mote/hardware-configuration.nix
+++ hosts/mote/hardware-configuration.nix
@@ -1,4 +1,5 @@
-{ modulesPath, ... }: {
+{ modulesPath, ... }:
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     ../../modules/netbootable_nfs.nix
@@ -14,4 +15,9 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
+  # new ddr4? in this economy?
+  boot.kernelParams = [
+    "memmap=32M$0x468000000"
+  ];
 }
